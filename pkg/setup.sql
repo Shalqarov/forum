ef1bf933b70f9fc102bb0bcc8451102776c97f11
Shalqarov <student@ALEM-316f06.alem.school>
2022-03-09 23:27:20 +0600

    added: post Table

--- pkg/setup.sql
+++ pkg/setup.sql
@@ -1,7 +1,16 @@
+-- users TABLE --
 CREATE TABLE IF NOT EXISTS "users" (
     "id" INTEGER NOT NULL UNIQUE,
     "username" TEXT NOT NULL UNIQUE,
     "email" TEXT NOT NULL UNIQUE,
     "password" TEXT NOT NULL,
     PRIMARY KEY("id" AUTOINCREMENT)
+);
+-- post TABLE --
+CREATE TABLE IF NOT EXISTS "post" (
+    "id" INTEGER NOT NULL UNIQUE PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "title" TEXT NOT NULL UNIQUE,
+    "content" TEXT NOT NULL,
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES user(id)
 );
