86b1f799ddf6a3e277dd06861a7d084205702cd0
Shalqarov <student@ALEM-316f4e.alem.school>
2022-03-21 23:14:16 +0600

    fixes

--- web/templates.go
+++ web/templates.go
@@ -14,10 +14,8 @@ type templateData struct {
 }
 
 func NewTemplateCache(dir string) (map[string]*template.Template, error) {
-	// Инициализирую новую карту, которая будет хранить кэш.
 	cache := map[string]*template.Template{}
 
-	// filepath.Glob - получает срез всех файловых путей с расширением "page.html".
 	pages, err := filepath.Glob(filepath.Join(dir, "*.page.html"))
 	if err != nil {
 		return nil, err
@@ -31,19 +29,16 @@ func NewTemplateCache(dir string) (map[string]*template.Template, error) {
 			return nil, err
 		}
 
-		// Использую метод ParseGlob для добавления всех каркасных шаблонов.
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.layout.html"))
 		if err != nil {
 			return nil, err
 		}
 
-		// Использую метод ParseGlob для добавления всех вспомогательных шаблонов.
 		ts, err = ts.ParseGlob(filepath.Join(dir, "*.partial.html"))
 		if err != nil {
 			return nil, err
 		}
 
-		// Добавляю полученный набор шаблонов в кэш.
 		cache[name] = ts
 	}
 	return cache, nil
