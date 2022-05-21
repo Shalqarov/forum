e246578bc75c400287f54b21f1717ed40114ad98
Shalqarov <Temirlan200312@gmail.com>
2022-05-21 12:12:52 +0600

    fix: GetCommentsByPostID returns correct errors

--- usecase/comment_usecase.go
+++ usecase/comment_usecase.go
@@ -23,13 +23,13 @@ func (u *commentUsecase) CreateComment(comm *domain.Comment) error {
 func (u *commentUsecase) GetCommentsByPostID(id int64) ([]*domain.Comment, error) {
 	comments, err := u.repo.GetCommentsByPostID(id)
 	if err != nil {
-		return nil, fmt.Errorf("GetPostByID error: %w", err)
+		return nil, fmt.Errorf("GetCommentsByPostID: %w", err)
 	}
 
 	for _, comment := range comments {
 		votes, err := u.GetVotesCountByCommentID(comment.ID)
 		if err != nil {
-			return nil, fmt.Errorf("GetVotesCountByCommentID error: %w", err)
+			return nil, fmt.Errorf("GetVotesCountByCommentID: %w", err)
 		}
 		comment.Votes = *votes
 	}
