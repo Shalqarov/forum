e0df4bfb74ef568e6edc0d9892c5d4afb3719359
Shalqarov <Temirlan200312@gmail.com>
2022-07-05 19:22:27 +0600

    add: githubLoginHandler dunction

--- web/github_handler.go
+++ web/github_handler.go
@@ -1,1 +1,20 @@
 package web
+
+import (
+	"fmt"
+	"net/http"
+)
+
+const (
+	githubClientID     = "e6775f24b7ae5d23a9bf"
+	githubClientSecret = "9631fe307239dd7150112696a6aaf3557aac79c9"
+)
+
+func (app *Handler) githubLoginHandler(w http.ResponseWriter, r *http.Request) {
+	redirectURL := fmt.Sprintf(
+		"https://github.com/login/oauth/authorize?client_id=%s&redirect_uri=%s&scope=user:email",
+		githubClientID,
+		"http://localhost:5000/signin/github/callback",
+	)
+	http.Redirect(w, r, redirectURL, http.StatusSeeOther)
+}
